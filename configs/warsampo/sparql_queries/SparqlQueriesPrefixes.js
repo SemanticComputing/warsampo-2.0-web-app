029060d606f5311d532203bddfb5b59d0fdfe9df
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-04-20 16:14:48 +0300

    Add basic configuration

--- configs/warsampo/sparql_queries/SparqlQueriesPrefixes.js
+++ configs/warsampo/sparql_queries/SparqlQueriesPrefixes.js
@@ -10,15 +10,8 @@ export const prefixes = `
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX sch: <http://schema.org/>
-  PREFIX geosparql: <http://www.opengis.net/ont/geosparql#>
-  PREFIX frbroo: <http://erlangen-crm.org/efrbroo/>
-  PREFIX mmm-schema: <http://ldf.fi/schema/mmm/>
   PREFIX gvp: <http://vocab.getty.edu/ontology#>
   PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-  PREFIX spatial: <http://jena.apache.org/spatial#>
-  PREFIX sd: <http://www.w3.org/ns/sparql-service-description#>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-  PREFIX h-schema: <http://ldf.fi/schema/hellerau/> 
-  PREFIX gn: <http://www.geonames.org/ontology#>
-  PREFIX semparls: <http://ldf.fi/schema/semparl/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX warsa: <http://ldf.fi/schema/warsa/>
 `
