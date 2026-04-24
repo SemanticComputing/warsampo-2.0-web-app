4812b3a637ceed0f676059ad141d4b5af5843283
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-04-24 15:02:52 +0300

    Add map visualization to cemeteries pespective

--- configs/warsampo/sparql_queries/SparqlQueriesCemeteries.js
+++ configs/warsampo/sparql_queries/SparqlQueriesCemeteries.js
@@ -142,3 +142,45 @@ export const cemeteryPropertiesInstancePage = `
       FILTER(LANG(?source__prefLabel) = '<LANG>')
     }
 `
+
+
+export const cemeteryPlacesQuery = `
+  SELECT ?id ?lat ?long
+  (COUNT(DISTINCT ?cemetery) as ?instanceCount)
+  WHERE {
+    <FILTER>
+    ?cemetery a warsa:Cemetery  ;
+              wgs84:lat ?lat ;
+              wgs84:long ?long .
+    FILTER(datatype(?lat) = xsd:float)
+    FILTER(datatype(?long) = xsd:float)
+    BIND(?cemetery as ?id)
+  }
+  GROUP BY ?id ?lat ?long
+`
+
+export const placePropertiesInfoWindow = `
+  ?id skos:prefLabel ?prefLabel__id .
+  BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+  BIND(CONCAT("/cemeteries/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+
+  OPTIONAL {
+    ?id cemeteries:number_of_graves ?numberOfGraves .
+  }
+
+  OPTIONAL {
+    ?image__id crm-org:P138_represents ?id ;
+                crm-org:P138i_has_representation ?image__representation .
+    ?image__representation photographs:size photographs:sm ;
+                          sch:contentUrl ?image__url .
+  }
+`
+
+export const peopleBuriedAt = `
+  OPTIONAL {
+    <FILTER>
+    ?related__id warsa:buried_in ?id .
+    ?related__id skos:prefLabel ?related__prefLabel .
+    BIND(CONCAT("/casualties/page/", REPLACE(STR(?related__id), "^.*\\\\/(.+)", "$1")) AS ?related__dataProviderUrl)
+  }
+`
