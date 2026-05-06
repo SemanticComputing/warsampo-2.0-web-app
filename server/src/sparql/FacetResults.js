4c9df7e1937b8db1dd9c69681b6a8b9f7985ca92
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-05-06 18:53:42 +0300

    Add possibility to include <SUBQUERY> in queries

--- server/src/sparql/FacetResults.js
+++ server/src/sparql/FacetResults.js
@@ -34,6 +34,21 @@ export const getPaginatedResults = ({
     resultMapperConfig = null,
     postprocess = null
   } = resultClassConfig.paginatedResultsConfig
+  q = q.replaceAll('<RESULT_SET_PROPERTIES>', propertiesQueryBlock)
+  q = q.replaceAll('<SUBQUERY>', `
+    {
+      # score and literal are used only for Jena full text index
+      SELECT DISTINCT ?id ?orderBy {
+        <FILTER>
+        VALUES ?facetClass { <FACET_CLASS> }
+        ?id <FACET_CLASS_PREDICATE> ?facetClass .
+        <ORDER_BY_TRIPLE>
+      }
+      <ORDER_BY>
+      <PAGE>
+    }
+    FILTER(BOUND(?id))
+  `)
   if (constraints == null && defaultConstraint == null) {
     q = q.replaceAll('<FILTER>', '# no filters')
   } else {
@@ -75,7 +90,6 @@ export const getPaginatedResults = ({
     q = q.replaceAll(/<FACET_CLASS_PREDICATE>/g, 'a')
   }
   q = q.replaceAll('<PAGE>', `LIMIT ${pagesize} OFFSET ${page * pagesize}`)
-  q = q.replaceAll('<RESULT_SET_PROPERTIES>', propertiesQueryBlock)
   if (langTag) {
     q = q.replaceAll(/<LANG>/g, langTag)
   }
