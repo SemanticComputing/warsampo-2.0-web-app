f1744d4ef4bb805673df650b10d45e66a065caf6
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-09-04 14:04:56 +0300

    Add actor instance pages

---
+++ configs/warsampo/sparql_queries/SparqlQueriesActors.js
@@ -0,0 +1,295 @@
+const perspectiveID = 'actors'
+
+export const actorPropertiesInstancePage = `
+    {
+      BIND(<ID> AS ?id)
+      ?id skos:prefLabel ?prefLabel__id .
+      BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(?id as ?uri__id)
+      BIND(?id as ?uri__dataProviderUrl)
+      BIND(?id as ?uri__prefLabel)
+      BIND(CONCAT("https://www.sotasampo.fi/fi/persons/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?warsaPage__id)
+      BIND(?warsaPage__id AS ?warsaPage__prefLabel)
+      BIND(?warsaPage__id AS ?warsaPage__dataProviderUrl)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord .
+      BIND(?deathRecord__id AS ?deathRecord__prefLabel)
+      BIND(?deathRecord__id AS ?deathRecord__dataProviderUrl)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(casualties:rank|prisoners:rank) ?rank__id .
+      ?rank__id skos:prefLabel ?rank__prefLabel .
+      FILTER(LANG(?rank__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id bioc:has_occupation ?occupation__id .
+      ?occupation__id skos:prefLabel ?occupation__prefLabel .
+      FILTER(LANG(?occupation__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:gender ?gender__id .
+      ?gender__id skos:prefLabel ?gender__prefLabel .
+      FILTER(LANG(?gender__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(warsa:marital_status|prisoners:marital_status) ?maritalStatus__id .
+      ?maritalStatus__id skos:prefLabel ?maritalStatus__prefLabel .
+      FILTER(LANG(?maritalStatus__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      casualties:perishing_category ?perishingCategory__id .
+      ?perishingCategory__id skos:prefLabel ?perishingCategory__prefLabel .
+      FILTER(LANG(?perishingCategory__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:buried_in ?buriedIn__id .
+      ?buriedIn__id skos:prefLabel ?buriedIn__prefLabel .
+      BIND(CONCAT("/cemeteries/page/", REPLACE(STR(?buriedIn__id), "^.*\\\\/(.+)", "$1")) AS ?buriedIn__dataProviderUrl)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(casualties:municipality_of_domicile|prisoners:municipality_of_domicile) ?municipalityOfDomicile__id .
+      ?municipalityOfDomicile__id skos:prefLabel ?municipalityOfDomicile__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      casualties:municipality_of_death ?municipalityOfDeath__id .
+      ?municipalityOfDeath__id skos:prefLabel ?municipalityOfDeath__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(warsa:date_of_death|prisoners:date_of_death) ?deathTime .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(casualties:unit|prisoners:unit) ?unit__id .
+      ?unit__id skos:prefLabel ?unit__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(casualties:municipality_of_residence|prisoners:municipality_of_residence) ?municipalityOfResidence__id .
+      ?municipalityOfResidence__id skos:prefLabel ?municipalityOfResidence__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      casualties:municipality_of_burial ?municipalityOfBurial__id .
+      ?municipalityOfBurial__id skos:prefLabel ?municipalityOfBurial__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:place_of_wounding ?placeOfWounding .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:date_of_wounding ?dateOfWounding .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      casualties:place_of_burial_number ?placeOfBurialNumber .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      casualties:unit_code ?unitCode .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(warsa:number_of_children|prisoners:number_of_children) ?numberOfChildren .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(casualties:municipality_of_birth|warsa:municipality_of_birth) ?municipalityOfBirth__id .
+      ?municipalityOfBirth__id skos:prefLabel ?municipalityOfBirth__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/warsa:date_of_birth ?dateOfBirth .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:citizenship ?citizenship__id .
+      ?citizenship__id skos:prefLabel ?citizenship__prefLabel .
+      FILTER(LANG(?citizenship__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?deathRecord__id .
+      ?deathRecord__id a warsa:DeathRecord ;
+                      warsa:nationality ?nationality__id .
+      ?nationality__id skos:prefLabel ?nationality__prefLabel .
+      FILTER(LANG(?nationality__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/warsa:mother_tongue ?motherTongue__id .
+      ?motherTongue__id skos:prefLabel ?motherTongue__prefLabel .
+      FILTER(LANG(?motherTongue__prefLabel) = '<LANG>')
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id foaf:page ?externalPage__id .
+      BIND(?externalPage__id AS ?externalPage__prefLabel)
+      BIND(?externalPage__id AS ?externalPage__dataProviderUrl)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id ^articles:mentionsPerson ?article__id .
+
+      ?article__id dce:title ?article__title ;
+                    articles:issue/skos:prefLabel ?article__issue ;
+                    dct:hasFormat ?article__dataProviderUrl .
+      
+      BIND(CONCAT(STR(?article__title), " (Kansa Taisteli ", STR(?article__issue), ")") AS ?article__prefLabel)
+    }
+    UNION 
+    {
+      BIND(<ID> AS ?id)
+      ?disappearingEvent__id a warsa:Disappearing ;
+                        crm-org:P11_had_participant  ?id .
+      BIND(?disappearingEvent__id AS ?disappearingEvent__prefLabel)
+      BIND(?disappearingEvent__id AS ?disappearingEvent__dataProviderUrl)
+    }
+    UNION 
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/casualties:municipality_of_going_mia ?municipalityOfGoingMIA__id .
+      ?municipalityOfGoingMIA__id skos:prefLabel ?municipalityOfGoingMIA__prefLabel .
+    }
+    UNION 
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(warsa:place_of_going_mia_literal|prisoners:place_of_going_mia_literal) ?placeOfGoingMIA .
+    }
+    UNION 
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in/(warsa:date_of_going_mia|prisoners:date_of_going_mia) ?dateOfGoingMIA .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord .
+      BIND(?prisonerRecord__id AS ?prisonerRecord__prefLabel)
+      BIND(?prisonerRecord__id AS ?prisonerRecord__dataProviderUrl)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:description_of_capture ?descriptionOfCapture .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:additional_information ?otherCaptivityInformation .
+    }                   
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:municipality_of_capture ?municipalityOfCapture__id .
+      ?municipalityOfCapture__id skos:prefLabel ?municipalityOfCapture__prefLabel .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          warsa:sotilaan_aani_magazine ?sotilaanAani__id .
+      ?sotilaanAani__id skos:prefLabel ?sotilaanAani__prefLabel ;
+                        sch:contentUrl ?sotilaanAani__dataProviderUrl .
+
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:captivity ?captivity__id .
+      
+      OPTIONAL { 
+        ?captivity__id prisoners:date_begin ?startDate .
+        BIND(CONCAT(STR(DAY(?startDate)), '.', STR(MONTH(?startDate)), '.', STR(YEAR(?startDate))) AS ?formattedStartDate)
+      }
+      OPTIONAL { 
+        ?captivity__id prisoners:date_end ?endDate . 
+        BIND(CONCAT(STR(DAY(?endDate)), '.', STR(MONTH(?endDate)), '.', STR(YEAR(?endDate))) AS ?formattedEndDate)
+      }
+
+      ?captivity__id prisoners:location/skos:prefLabel ?locationName .
+      BIND(IF(BOUND(?formattedStartDate) && BOUND(?formattedEndDate), CONCAT(STR(?locationName), ' (', STR(?formattedStartDate), '–', STR(?formattedEndDate), ')'), STR(?locationName)) AS ?captivity__prefLabel)
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:date_of_capture ?dateOfCapture .
+    }
+    UNION
+    {
+      BIND(<ID> AS ?id)
+      ?id crm-org:P70i_is_documented_in ?prisonerRecord__id .
+      ?prisonerRecord__id a warsa:PrisonerRecord ;
+                          prisoners:date_of_return ?dateOfReturn .
+    }
+`
